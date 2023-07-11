be5babdab6ec485f3fae30a60e9540d46d60c656
Mohit <109670799+Mohit-au50@users.noreply.github.com>
2023-07-11 19:14:35 +0530

    latest changes

---
+++ Models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = model("B_User", UserSchema);
+module.exports = User;
